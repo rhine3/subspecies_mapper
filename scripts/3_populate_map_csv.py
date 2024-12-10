cac5d24ae8d7207cbafdf3509f4090b574ba652c
Tessa Rhinehart <tessa.rhinehart@pitt.edu>
2024-12-10 06:09:05 -0500

    Print number of species at the end

--- scripts/3_populate_map_csv.py
+++ scripts/3_populate_map_csv.py
@@ -6,11 +6,12 @@ taxonomy = pd.read_csv("../resources/eBird_taxonomy_v2024.csv")
 
 ## Create a CSV of map URLs for the website
 df = pd.DataFrame(columns=["common_name", "scientific_name", "resolution", "map_url"])
-maps_dir = Path("docs/maps")
+maps_dir = Path("../docs/maps")
 
 # Sort maps taxonomically
 maps_list = list(maps_dir.glob("*.html"))
 species = {p.name.split('_')[0].replace('-', ' '): p for p in maps_list}
+total_spp = len(species.keys())
 ordering = taxonomy[['SCI_NAME', 'TAXON_ORDER']].set_index("SCI_NAME").to_dict()['TAXON_ORDER']
 maps_list = sorted(maps_list, key=lambda x: ordering[x.name.split('_')[0].replace('-', ' ')])
 
@@ -25,3 +26,5 @@ for idx, file in enumerate(maps_list):
     print(map_url)
     df.loc[idx] = [common_name, species, resolution, map_url]
 df.to_csv("../docs/data/map_data.csv", index=False)
+
+print("Number of species mapped:", total_spp)
